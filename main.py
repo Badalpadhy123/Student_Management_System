068230a098a7bf7dfd04236b6328b9a1053ed6b7
Badalpadhy123 <badalpadhy2005@gmail.com>
2026-07-11 18:51:23 +0530

    code intialized

---
+++ main.py
@@ -0,0 +1,14 @@
+while True:
+    print("\n===== Student Management System =====")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Update Student")
+    print("5. Delete Student")
+    print("6. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "6":
+        print("Thank you!")
+        break
